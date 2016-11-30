62d146c42215b3b78931c85671ea6f11c8c11ef4
ekoshmarik <ekoshmarik@gmail.com>
2016-11-30 17:20:38 +0200

    added openwethermap api key & redux-promise

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,15 +4,22 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+    //this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     console.log(event.target.value);
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in some cities"
           className="form-control"
